c07cf350d2c3038bdd67e0d314dddb057bff973e
Frank Murphy <none@none>
2011-04-29 20:32:59 -0400

    Added a generator-based alternative to sequencing.

--- test/test_basic.py
+++ test/test_basic.py
@@ -12,13 +12,35 @@ def withClient(f):
         return defer.addCallback(cb)
     return _inner
 
+def sequence(f):
+    """
+    Sequences a generator function through deferreds. Takes each sent 
+    ``Deferred`` and attaches a callback to it that sends the generator
+    function the results when they arrive. Continues to recursively do this
+    until the generator function is exhausted.
+
+    """
+    def _inner(*args):
+        gen = f(*args)
+        first = gen.next()
+        def cb(*args):
+            if len(args) == 1: args = args[0]
+            try:
+                d = gen.send(args)
+                return d.addCallback(cb)
+            except StopIteration:
+                pass
+    
+        return first.addCallback(cb)
+    
+    return _inner
+
 class ClientHelloTest(unittest.TestCase):
     @withClient
+    @sequence
     def test_hello(self, client):
-        defer = client.deferred['NodeHello']
-        def check_version(msg):
-            self.assertEquals(msg['FCPVersion'], '2.0')
-        return defer.addCallback(check_version)
+        msg = yield client.deferred['NodeHello']
+        self.assertEquals(msg['FCPVersion'], '2.0')
 
     def tearDown(self):
         if self.client is not None:
