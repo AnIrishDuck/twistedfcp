2cf91367a6378f0ffc79ab3b9b3c5ae5cfa33191
Frank Murphy <none@none>
2011-04-29 20:17:08 -0400

    Cleaned up basic test a little.

--- test/test_basic.py
+++ test/test_basic.py
@@ -2,20 +2,23 @@ from twisted.internet import reactor, protocol
 from twisted.trial import unittest
 from twistedfcp.protocol import fcp_protocol
 
-class ClientHelloTest(unittest.TestCase):
-
-    def log_message(self, msg):
-        self.assertEquals(msg['FCPVersion'], '2.0')
- 
-    def test_hello(self):
-        creator = protocol.ClientCreator(reactor, fcp_protocol) 
+def withClient(f):
+    def _inner(self):
+        creator = protocol.ClientCreator(reactor, fcp_protocol)
         def cb(client):
             self.client = client
-            defer = client.deferred['NodeHello']
-            return defer.addCallback(self.log_message)
-
+            return f(self, client)
         defer = creator.connectTCP('localhost', fcp_protocol.port)
         return defer.addCallback(cb)
+    return _inner
+
+class ClientHelloTest(unittest.TestCase):
+    @withClient
+    def test_hello(self, client):
+        defer = client.deferred['NodeHello']
+        def check_version(msg):
+            self.assertEquals(msg['FCPVersion'], '2.0')
+        return defer.addCallback(check_version)
 
     def tearDown(self):
         if self.client is not None:
