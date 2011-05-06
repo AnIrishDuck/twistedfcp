510f7f306a58c956514bb5050385de8193c59acf
Frank Murphy <fpmurphy@mtu.edu>
2011-05-05 22:21:40 -0400

    Split off a Put exception.

--- twistedfcp/error.py
+++ twistedfcp/error.py
@@ -5,8 +5,8 @@ transform raw messages into Python ``Exception`` objects.
 """
 from twisted.python.failure import Failure
 
-class FetchException(Failure, Exception):
-    "An exception indicating a prior fetch operation has failed."
+class FCPException(Failure, Exception):
+    "Base exception for all FCP errors."
 
     def __init__(self, error_msg):
         self.code = int(error_msg['Code'])
@@ -17,3 +17,10 @@ class FetchException(Failure, Exception):
     def __repr__(self): return "{0}: {1}".format(type(self), self.msg)
 
     def __str__(self): return self.__repr__()
+
+class FetchException(FCPException):
+    "An exception indicating a prior fetch operation has failed."
+
+class PutException(FCPException):
+    "An exception indicating a prior put operation has failed."
+
