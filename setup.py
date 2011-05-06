53c4529b3558dc5fc389d9942c7d91bbdd1fe156
Frank Murphy <fpmurphy@mtu.edu>
2011-05-06 18:41:22 -0400

    Added simple setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='twistedfcp',
+    version='0.1dev',
+    author='Frank Murphy',
+    packages=['twistedfcp'],
+    license='LGPL v3',
+    long_description=open('README.rst').read()
+)
